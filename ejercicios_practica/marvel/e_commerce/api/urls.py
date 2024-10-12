6b64208618a7e17579f2a3451f4b351f87af34cc
FChamorroMendoza <138070520+FChamorroMendoza@users.noreply.github.com>
2024-10-12 18:29:16 -0500

    Add files via upload

---
+++ ejercicios_practica/marvel/e_commerce/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from e_commerce.api.views import *
+
+
+urlpatterns = [
+    path('comic-list/', comic_list_api_view),
+    path('comic-retrieve/', comic_retrieve_api_view),
+    path('comic-create/', comic_create_api_view),
+    path('comic-filtered/', comic_list_filtered_api_view),
+    path('comic-filtered-less/', comic_list_filtered_less_api_view)
+
+    
+]
